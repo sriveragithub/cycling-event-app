b749272ae1a13fce70b9667d8591f1dddb9fe33b
Skyler Rivera <skylerrivera17@yahoo.com>
2021-04-09 23:24:15 -0400

    initial commit, includes filter component

---
+++ src/Components/FilterEvent.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class FilterEvent extends React.Component {
+    render() {
+        return (
+            <div className='filter-events'>
+                <h3>Filter Events</h3>
+                <div className='filter-input'>
+                    <select placeholder='Select Event Typre...'>
+                        <option>Road Race</option>
+                        <option>Gravel Race</option>
+                        <option>Cyclocross</option>
+                        <option>Track Race</option>
+                        <option>Criterium</option>
+                    </select>
+                </div>
+                <div className='filter-input'>
+                    <input name='event-name' placeholder='Event Name...'></input>
+                </div>
+                <div className='filter-input'>
+                    <input name='event-state' placeholder='State...'></input>
+                </div>
+                <div className='filter-input'>
+                    <input name='event-city' placeholder='City...'></input>
+                </div>
+                <div className='filter-input'>
+                    <button type='button'>Submit</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FilterEvent
